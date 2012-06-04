0b9dbf4fa01aac57be3d1d58051de6ab700bd2bd
Benjamin Sonntag <benjamin@sonntag.fr>
2012-06-04 21:07:59 +0200

    changing piplome/pishirt for bigger versions

--- 31_donation_form1_html.php
+++ 31_donation_form1_html.php
@@ -30,29 +30,28 @@
                                     <label for="sum10">10&nbsp;&euro;</label>
 
                                     <div class="alert" id="piplome_pdf" style="display:none">
-                                        <p class="cadeaux"><img src="images/upiplomepdf.png"></p>
                                         <p>En remerciement de votre don, vous recevrez 1 000 décimales de π 
                                             dans un certificat personnalisé, envoyé <strong>par mail au format PDF</strong>.</p>
+                                        <p class="cadeaux"><img src="images/piplomepdf.png"></p>
                                     </div>
                             </div>
                             <div class="span1 amounts_holder">
                                     <input type="radio" name="sum" value="25" id="sum25"<?php ifcheck($_REQUEST["sum"]=="25"); ?> class="predef" />
                                     <label for="sum25">25&nbsp;&euro;</label>
                                     <div class="alert" id="piplome_pdf2" style="display:none">
-                                        <p class="cadeaux"><img src="images/upiplomepdf.png"></p>
                                         <p>En remerciement de votre don, vous recevrez 1 000 décimales de π 
                                             dans un certificat personnalisé, envoyé <strong>par mail au format PDF</strong>.</p>
+                                        <p class="cadeaux"><img src="images/piplomepdf.png"></p>
                                     </div>
                             </div>
                             <div class="span1 amounts_holder">
                                     <input type="radio" name="sum" value="50" id="sum50"<?php ifcheck($_REQUEST["sum"]=="50"); ?> class="predef" />
                                     <label for="sum50">50&nbsp;&euro;</label>
                                     <div class="alert " id="piplome" style="display:none">
-                                        <p class="cadeaux"><img src="images/upiplome.png"></p>
                                         <p>En remerciement de votre don, vous recevrez 1 000 décimales de π 
                                             dans un certificat personnalisé, envoyé <strong>par courriel au format PDF 
-                                                + un  tirage original de qualité supérieure</strong>.
-                                        </p>
+                                                + un  tirage original de qualité supérieure</strong>.</p>
+                                        <p class="cadeaux"><img src="images/piplomemail.png"></p>
                                     </div>    
                             </div>
 
@@ -60,27 +59,29 @@
                                     <input type="radio" name="sum" value="100" id="sum100"<?php ifcheck($_REQUEST["sum"]=="100"); ?> class="predef" />
                                     <label for="sum100">100&nbsp;&euro;</label>
                                     <div class="alert " id="piplome_ts"  style="display:none">
-                                        <p class="cadeaux"><img src="images/upiplome.png">
-                                            <span class="plus">+</span><img src="images/upishirt.png"></p>
                                         <p>En remerciement de votre don, vous recevrez 1 000 
                                             décimales de π dans un certificat personnalisé, 
                                             envoyé par courriel au format PDF + un  
                                             tirage original de qualité supérieure + un 
                                             'pi-shirt' portant le logo de La Quadrature 
-                                            du Net !</p></div>    
+                                            du Net !</p>
+                                        <p class="cadeaux"><img src="images/piplomemail.png">
+                                            <img src="images/pishirt.png"></p>
+				    </div>    
                             </div>
                             <div class="span1 amounts_holder">
                                     <input type="radio" name="sum" value="250" id="sum250"<?php ifcheck($_REQUEST["sum"]=="25"); ?> class="predef" />
                                     <label for="sum250">250&nbsp;&euro;</label>
                                     <div class="alert " id="piplome_ts2"  style="display:none">
-                                        <p class="cadeaux"><img src="images/upiplome.png">
-                                            <span class="plus">+</span><img src="images/upishirt.png"></p>
                                         <p>En remerciement de votre don, vous recevrez 1 000 
                                             décimales de π dans un certificat personnalisé, 
                                             envoyé par courriel au format PDF + un  
                                             tirage original de qualité supérieure + un 
                                             'pi-shirt' portant le logo de La Quadrature 
-                                            du Net !</p></div>    
+                                            du Net !</p>
+                                        <p class="cadeaux"><img src="images/piplomemail.png">
+                                            <img src="images/pishirt.png"></p>
+				    </div>    
                             </div>
                             <div class="span1 amounts_holder othersum">
 				    <input type="radio" name="sum" value="-1" id="sum1"<?php ifcheck(!in_array($_REQUEST["sum"],$asums)); ?> />
@@ -98,11 +99,6 @@
                         </div>                            
                     </div>                            
                     <div class="row">
-                        <div class="span6">
-                                <div class="giftContainer ui-widget-content" style="display:none;"></div>
-                        </div>                            
-                    </div>                            
-                    <div class="row">
                         <div class="span6 checkbox_holder">
                                     <input type="checkbox" id="monthly" name="monthly"<?php ifcheck($_REQUEST["monthly"]); ?>/> <label for="monthly"><?php __("Make this a monthly donation (<b>french bank accounts only</b>)"); ?></label>
                         </div>
@@ -113,5 +109,10 @@
                                 <input type="submit" class="btn btn-large btn-inverse" value="<?php __("I support La Quadrature du Net"); ?> &gt;&gt;"/>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="span6">
+                                <div class="giftContainer ui-widget-content" style="display:none;"></div>
+                        </div>                            
+                    </div>                            
                     </form><!-- donation form -->
                 </div>
