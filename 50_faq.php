d9f2c185942e1810639157f600daf4ca2360af50
Benjamin Sonntag <benjamin@sonntag.fr>
2012-06-08 02:00:52 +0200

    FAQ now translatable

--- 50_faq.php
+++ 50_faq.php
@@ -10,67 +10,65 @@
                             });
                             </script>
 
-			<h4 id="faq-budget"><a href="#faq-budget">Where will my money go?</a></h4>
+			<h4 id="faq-budget"><a href="#faq-budget"><?php __("Where will my money go?"); ?></a></h4>
 			<div><img src="/images/pichart_en2012.png"/>
 			<br/>
 			<!--<br/>Forecasted Budget for 2012: 201&nbsp;K &euro;-->
 			</div>
-			<h4  id="faq-nomoney"><a href="#faq-nomoney">I don't have any money,  how can I help La Quadrature du Net?</a></h4>
+			<h4  id="faq-nomoney"><a href="#faq-nomoney"><?php __("I don't have any money,  how can I help La Quadrature du Net?"); ?></a></h4>
 			<div>
-			    <br/>Spread the word about this campaign! By speaking up on social networks, on blogs, or by telling people around you about the importance of La Quadrature's actions,  and how they can participate, you can help us. This support is vital, as La Quadrature can only exist through the citizens who share our commitment to the free Internet.
-			    <br/>You can also support La Quadrature du Net day after day by <a target="_blank" href="http://laquadrature.net/en/how-to-participate">getting involved</a>.
-			    <br/>Financial donations help la Quadrature work on a day-to-day basis by paying for its fulltime activists'  salaries, costs and such, but la Quadrature would be nothing without the people who give their time, skills and energy to support its actions  and make them their own.
-			    <br/>Please don't hesitate to <a target="_blank" href="http://www.laquadrature.net/en/contact-us">get in touch</a> to find a way to contribute that suits you!
+			    <br/><?php __("Spread the word about this campaign! By speaking up on social networks, on blogs, or by telling people around you about the importance of La Quadrature's actions,  and how they can participate, you can help us. This support is vital, as La Quadrature can only exist through the citizens who share our commitment to the free Internet."); ?>
+			    <br/><?php __("You can also support La Quadrature du Net day after day by <a target=\"_blank\" href=\"http://laquadrature.net/en/how-to-participate\">getting involved"); ?></a>.
+			    <br/><?php __("Financial donations help la Quadrature work on a day-to-day basis by paying for its fulltime activists'  salaries, costs and such, but la Quadrature would be nothing without the people who give their time, skills and energy to support its actions  and make them their own."); ?>
+			    <br/><?php __("Please don't hesitate to <a target=\"_blank\" href=\"http://www.laquadrature.net/en/contact-us\">get in touch</a> to find a way to contribute that suits you!"); ?>
 			</div>
-			<h4 id="faq-tax"><a href="#faq-tax">Is my support tax-deductible?</a> </h4>
+			<h4 id="faq-tax"><a href="#faq-tax"><?php __("Is my support tax-deductible?"); ?></a> </h4>
 			<div id="faq5" >
-			  Not at the moment. We are looking into a way of making this possible, but unfortunately it is not possible for current donations.
+			  <?php __("Not at the moment. We are looking into a way of making this possible, but unfortunately it is not possible for current donations."); ?>
 			</div>
-			<h4 id="faq-recu"><a href="#faq-recu">I need a receipt or an invoice for my donation.</a> </h4>
+			<h4 id="faq-recu"><a href="#faq-recu"><?php __("I need a receipt or an invoice for my donation."); ?></a> </h4>
 			<div id="faq6" > 
-			  We can send you a receipt for accounting purposes. Please <a target="_blank" href="http://www.laquadrature.net/en/contact-us">contact us</a> by email.
+			  <?php __("We can send you a receipt for accounting purposes. Please <a target=\"_blank\" href=\"http://www.laquadrature.net/en/contact-us\">contact us</a> by email."); ?>
 			</div>
-			<h4 id="faq-who"><a href="#faq-who">Who supports La Quadrature du Net?</a></h4>
+			<h4 id="faq-who"><a href="#faq-who"><?php __("Who supports La Quadrature du Net?"); ?></a></h4>
 			<div>
-			    Several <a target="_blank" href="http://www.laquadrature.net/en/they-support-la-quadrature-du-net">NGOs support La Quadrature</a>, as well as <a target="_blank" href="http://www.laquadrature.net/en/public-supporters">thousands of individuals who have made a donation</a> and wish to be listed publicly.
+			    <?php __("Several <a target=\"_blank\" href=\"http://www.laquadrature.net/en/they-support-la-quadrature-du-net\">NGOs support La Quadrature</a>, as well as <a target=\"_blank\" href=\"http://www.laquadrature.net/en/public-supporters\">thousands of individuals who have made a donation</a> and wish to be listed publicly."); ?>
 			</div>
-			<h4 id="faq-cheque"><a href="#faq-cheque">Can I make a donation by check?</a></h4>
+			<h4 id="faq-cheque"><a href="#faq-cheque"><?php __("Can I make a donation by check?"); ?></a></h4>
 			<div>
-			    Yes. To donate by check, stop the payment procedure just after you click on the “I support La Quadrature du Net” button. Once you reach the donation summary, do not click on the “proceed with your payment” button.
-			    Send your check to the following address:<br/>
+			    <?php __("Yes. To donate by check, stop the payment procedure when you are at our bank's website. Send your check to the following address:"); ?> <br />
 			    <br/><table class="formv"> 
-			      <tr><td><i>Payable to: </i></td><td>FDNN</td></tr> 
-			      <tr><td><i>Address: </i></td><td>FDNN, 16 rue de Cachy, 80090 Amiens</td></tr> 
-			      <tr><td><b>Important note: </b></td><td>Please write down the name you gave us on the back of your check in order to allow us to find you easily.</td></tr> 
+			      <?php __("<tr><td><i>Payable to: </i></td><td>FDNN</td></tr>"); ?>
+			      <?php __("<tr><td><i>Address: </i></td><td>FDNN, 16 rue de Cachy, 80090 Amiens</td></tr>"); ?>
+			      <?php __("<tr><td><b>Important note: </b></td><td>Please write down the name you gave us on the back of your check in order to allow us to find you easily.</td></tr>"); ?>
 			    </table> <br/>
-			    FDNN (Fond de Défense de la Net Neutralité, “Net Neutrality Defense Fund”), is the organisation which manages La Quadrature du Net's accounts.
+			    <?php __("FDNN (Fond de Défense de la Net Neutralité, “Net Neutrality Defense Fund”), is the organisation which manages La Quadrature du Net's accounts."); ?>
 			</div>
-			<h4 id="faq-virement-transfert"><a href="#faq-virement-transfert">Can I make a donation through money transfer?</a> </h4>
+			<h4 id="faq-virement-transfert"><a href="#faq-virement-transfert"><?php __("Can I make a donation through money transfer?"); ?></a> </h4>
 			<div id="faq0" > 
-			  Sure! You need to transfer your donation to FDNN's bank account (FDNN takes care of La Quadrature du Net's accounts) using the following details. <br/>
+			  <?php __("Sure! You need to transfer your donation to FDNN's bank account (FDNN takes care of La Quadrature du Net's accounts) using the following details."); ?> <br/>
 			<br/> 
-			  <b>International bank transfer: </b>IBAN: FR76 1027 8060 3100 0203 2270 187<br/>
-			  <br/>You can also <a target="_blank" href="http://www.laquadrature.net/images/RIB_FDNN.pdf">download the details of FDNN's bank account</a>. 
-			      <b>Important note: </b>Please quote the name you gave us as your transfer's comment, so we can send you your pi decimals or other goodies.
+			  <?php __("<b>International bank transfer: </b>IBAN: FR76 1027 8060 3100 0203 2270 187"); ?><br />
+			  <br/><?php __("You can also <a target=\"_blank\" href=\"http://www.laquadrature.net/images/RIB_FDNN.pdf\">download the details of FDNN's bank account</a>."); ?> 
+			      <?php __("<b>Important note: </b>Please quote the name you gave us as your transfer's comment, so we can send you your pi decimals or other goodies."); ?>
 			</div>
-			<h4 id="faq-paypal"><a href="#faq-paypal">Can I make a donation through PayPal?</a></h4>
-			<div>We  do not recommend using PayPal for donations, for ethical reasons (their actions against WikiLeaks, among others) and because of the overtly expensive costs incurred. Nevertheless, if it is your only option, you  may use the following account: paypal@laquadrature.net.
+			<h4 id="faq-paypal"><a href="#faq-paypal"><?php __("Can I make a donation through PayPal?"); ?></a></h4>
+			<div><?php __("We do not recommend using PayPal for donations, for ethical reasons (their actions against WikiLeaks, among others) and because of the overtly expensive costs incurred. Nevertheless, if it is your only option, you  may use the following account: paypal@laquadrature.net."); ?>
 			</div>
-			<h4 id="faq-flattr"><a href="#faq-flattr">Can I make a donation via Flattr?</a> </h4>
+			<h4 id="faq-flattr"><a href="#faq-flattr"><?php __("Can I make a donation via Flattr?"); ?></a> </h4>
 			<div id="faq10" > 
-			<br/>Yes! If you wish to support la Quadrature via Flattr, you can simply flatter this page, or any of the site's articles.
-			<br/>Flattr  is a great way to support us, but as a Flattr is quite small, please  don't hesitate to flatter our articles to your heart's content.
-<a href="http://flattr.com/thing/129120/La-Quadrature-du-Net"><img src="/files/flattr-compact.png" title="Flattr this" alt="Flattr" />
-</a>
-			<br/>Flattr  also offers the possibility to make larger donations, which offers an  interesting alternative to PayPal.<br/>To make a donation to La  Quadrature du Net via Flattr, please use  <a target="_blank" href="https://flattr.com/donation/give/to/laquadrature">the form on this page</a>. As Flattr donations are anonymous, we will not be able to send you your goodies, unless you <a target="_blank" href="http://www.laquadrature.net/en/contact-us">contact us</a> with identifiable information, and in particular the the amount you plan to give in your donation.
+			<br/><?php __("Yes! If you wish to support la Quadrature via Flattr, you can simply flatter this page, or any of the site's articles."); ?>
+			<br/><?php __("Flattr is a great way to support us, but as a Flattr is quite small, please  don't hesitate to flatter our articles to your heart's content.
+<a href=\"http://flattr.com/thing/129120/La-Quadrature-du-Net\"><img src=\"/files/flattr-compact.png\" title=\"Flattr this\" alt=\"Flattr\" /></a>"); ?>
+			<br/><?php __("Flattr also offers the possibility to make larger donations, which offers an  interesting alternative to PayPal.<br/>To make a donation to La  Quadrature du Net via Flattr, please use  <a target=\"_blank\" href=\"https://flattr.com/donation/give/to/laquadrature\">the form on this page</a>. As Flattr donations are anonymous, we will not be able to send you your goodies, unless you <a target=\"_blank\" href=\"http://www.laquadrature.net/en/contact-us\">contact us</a> with identifiable information, and in particular the the amount you plan to give in your donation."); ?>
 			</div>
-			<h4 id="faq-buy-t-shirts"><a href="#faq-buy-t-shirts">Can I support La Quadrature du Net by buying a t-shirt?</a> </h4>
+			<h4 id="faq-buy-t-shirts"><a href="#faq-buy-t-shirts"><?php __("Can I support La Quadrature du Net by buying a t-shirt?"); ?></a> </h4>
 			<div>
-			La Quadrature du Net does not sell t-shirts. The only way to support la Quadrature du Net (financially) is to make a donation. Of course, you can also help and support la Quadrature in other ways, by helping out for instance. Selling t-shirts would require a lot of time and effort and would probably mean hiring someone in order to make anything substantial from it. It would not be worth it. The pi-shirts are but a (small) gift from la Quadrature to show its gratitude and thank its most generous benefactors.
+			<?php __("La Quadrature du Net does not sell t-shirts. The only way to support la Quadrature du Net (financially) is to make a donation. Of course, you can also help and support la Quadrature in other ways, by helping out for instance. Selling t-shirts would require a lot of time and effort and would probably mean hiring someone in order to make anything substantial from it. It would not be worth it. The pi-shirts are but a (small) gift from la Quadrature to show its gratitude and thank its most generous benefactors."); ?>
 			</div>
-			<h4 id="faq-when-t-shirts"><a href="#faq-when-t-shirts">When will I get my shirt?</a> </h4>
+			<h4 id="faq-when-t-shirts"><a href="#faq-when-t-shirts"><?php __("When will I get my shirt?"); ?></a> </h4>
 			<div id="faq7" > 
-			For logistical reasons, t-shirts are sent in batch. This may take a while, sometimes up to a couple months. Please forgive us for this delay.
+			<?php __("For logistical reasons, t-shirts are sent in batch. This may take a while, sometimes up to a couple months. Please forgive us for this delay."); ?>
 			</div>
 
 
