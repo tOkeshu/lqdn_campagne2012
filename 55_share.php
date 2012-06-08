6e92f48e35c91c7821d043397bee2ee84051de97
alban <alban@albancrommer.com>
2012-06-08 13:34:18 +0200

    [ui] New facebook link system

--- 55_share.php
+++ 55_share.php
@@ -11,24 +11,28 @@
                     <div class="row">
                         <div class="span6">
 
-<div class="row"><div class="span2">
+                            <div class="row">
+                                <div class="span2">
+                                    <a class="btn-mini" href="https://identi.ca/notice/new?status_textarea=<?php echo urlencode(_("Like me, support @LaQuadrature du Net against #ACTA and beyond! https://support.laquadrature.net/")); ?>"><img src="images/sm_identica.png" /> Identi.ca</a>
+                                </div>
+                                <div class="span2">
+                                    <a class="btn-mini" href="http://twitter.com/home?status=<?php echo urlencode(_("Like me, support @LaQuadrature du Net against #ACTA and beyond! https://support.laquadrature.net/")); ?>"><img src="images/sm_twitter.png" /> Twitter</a>
 
-                        <a class="btn-mini" href="https://identi.ca/notice/new?status_textarea=<?php echo urlencode(_("Like me, support @LaQuadrature du Net against #ACTA and beyond! https://support.laquadrature.net/")); ?>"><img src="images/sm_identica.png" /> Identi.ca</a>
-
-</div><div class="span2">
-       <a class="btn-mini" href="http://twitter.com/home?status=<?php echo urlencode(_("Like me, support @LaQuadrature du Net against #ACTA and beyond! https://support.laquadrature.net/")); ?>"><img src="images/sm_twitter.png" /> Twitter</a>
-
-</div></div><div class="row"><div class="span2">
-
-</div><div class="span2">
-                          <a class="btn-mini" href="http://reddit.com/submit?url=<?php echo urlencode(ROOTURL); ?>&title=<?php echo urlencode(_("Like me, support La Quadrature du Net against #ACTA and beyond!")); ?>"><img src="images/sm_reddit.png" /> Reddit</a>
-
-</div><div class="span2">
-                            <a class="btn-mini" href="mailto:?subject=<?php echo urlencode(_("Support La Quadrature du Net against ACTA and beyond")) ?>&body=<?php echo urlencode(_("For the past four years, La Quadrature du Net has raised awareness and campaigned against ACTA. We need your help to defeat ACTA and move forward, advocating for a positive agenda to protect our freedoms. https://support.laquadrature.net/")) ?>"><img src="images/sm_mail.png" /> Email</a>
-</div></div><div class="row"><div class="span2">
-<div id="socialshareprivacy"></div>
-</div></div>
-
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="span2">
+                                    <a class="btn-mini" href="http://reddit.com/submit?url=<?php echo urlencode(ROOTURL); ?>&title=<?php echo urlencode(_("Like me, support La Quadrature du Net against #ACTA and beyond!")); ?>"><img src="images/sm_reddit.png" /> Reddit</a>
+                                </div>
+                                <div class="span2">
+                                    <a class="btn-mini" href="mailto:?subject=<?php echo urlencode(_("Support La Quadrature du Net against ACTA and beyond")) ?>&body=<?php echo urlencode(_("For the past four years, La Quadrature du Net has raised awareness and campaigned against ACTA. We need your help to defeat ACTA and move forward, advocating for a positive agenda to protect our freedoms. https://support.laquadrature.net/")) ?>"><img src="images/sm_mail.png" /> Email</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="span4">
+                                    <div id="socialshareprivacy"></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-</div>
+                </div>
