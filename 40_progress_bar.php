21d9d8bec69e808d9ff435c9a226de636f2572d1
root <root@pi.serverside.fr>
2012-06-11 16:48:44 +0200

    leger decalage de date, d'autres à prevoir

--- 40_progress_bar.php
+++ 40_progress_bar.php
@@ -1,5 +1,5 @@
 <?php
-	    list($obj1count)=@mysql_fetch_array(mysql_query("SELECT SUM(somme) FROM pi_mailing.dons WHERE status!=0  AND status!=100 AND datec > '2012-06-04'"));
+	    list($obj1count)=@mysql_fetch_array(mysql_query("SELECT SUM(somme) FROM pi_mailing.dons WHERE status!=0  AND status!=100 AND datec > '2012-04-04'"));
 ?>
 <a name="progressbar"></a>
     <div class="container-wrapper" id="progress"><!-- progress bar -->
@@ -9,7 +9,7 @@
                     <h3><?php __("Progress Bar"); ?></h3>
                 </div>-->
                 <div class="span10">
-	      <div id="progress_bar" title="<?php echo $obj1count." €, ".min(1,max(intval($obj1count/1000),100))." %"; ?>"></div>
+	      <div id="progress_bar" title="<?php echo $obj1count." €, ".intval($obj1count/1000)." %"; ?>"></div>
                     <div class="progress_indices">
                         <div class="marks">
                             <div class="start">0&euro;</div>
@@ -73,7 +73,7 @@ $(document).ready(function() {
   // Sets the progress bar
   $( "#progress_bar" ).progressbar({
   value: <?php 
-  echo min(1,max(intval($obj1count/1000),100)); 
+    echo intval($obj1count/1000);
 ?>
   
   });
