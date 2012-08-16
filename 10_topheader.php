d679d15f25c9b00c4fcfaacc91319f291c30fe56
Benjamin Sonntag <benjamin@sonntag.fr>
2012-08-16 12:04:59 +0200

    new texts

--- 10_topheader.php
+++ 10_topheader.php
@@ -8,7 +8,7 @@
                <!-- <div class="offset1 span9"> -->
 <div  style="float: right; padding-left: 30px; margin-left:-30px;"><a href="http://laquadrature.net"><img src="images/lqdn.png"/></a></div>
        <h1><a href="#a_support"><?php __("Support La Quadrature du Net"); ?></a></h1>
-       <p><?php __("For the past four years, La Quadrature du Net has raised awareness and campaigned against ACTA. <br/>We need your help to defeat ACTA and move forward, advocating for a positive agenda to protect our freedoms.");  ?> </p>
+       <p><?php __("For the past four years, La Quadrature du Net has raised awareness and campaigned against ACTA. <br/>By now, we need your help to build a post-ACTA world, advocating for a positive agenda to protect our freedoms.");  ?> </p>
 
                <!-- </div> -->
 
